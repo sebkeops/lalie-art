cf56edcb6638e34bb9a5d5d0042952a95523c531
sebkeops <siguenza010987@gmail.com>
2026-03-18 00:20:33 +0100

    Footer : suppression bloc copyright et lien admin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -47,13 +47,6 @@ export default function Footer() {
                         Musique, icônes, visages, histoires.
                     </div>
                 </div>
-
-                <div className="footerBottom">
-                    <span>© {new Date().getFullYear()} Crea Lalie Art</span>
-                    <a className="footerSmallLink" href="/admin/login">
-                        Admin
-                    </a>
-                </div>
             </div>
         </footer>
     );
